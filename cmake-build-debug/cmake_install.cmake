6639df2d0a8c24c5ddb4b99b2fccc40cdc69974f
Diego Viera <diegovieramartinez@gmail.com>
2024-09-07 02:17:46 +0200

    bug fixed

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/diegovieramartinez/Desktop/Space-Invader-UH
+# Install script for directory: /Users/diegovieramartinez/Desktop/sp/Space-Invader-UH
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/diegovieramartinez/Desktop/Space-Invader-UH/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/diegovieramartinez/Desktop/sp/Space-Invader-UH/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
